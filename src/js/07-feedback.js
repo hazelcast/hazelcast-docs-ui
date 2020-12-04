da599c9bc0d0cdea0d22aa2d60467729e39c85c7
JakeSCahill <jakecahillgh@gmail.com>
2020-12-04 10:05:04 +0000

    Add analytics code

--- src/js/07-feedback.js
+++ src/js/07-feedback.js
@@ -20,13 +20,13 @@
   yes.onclick = function () {
     options.innerHTML = ''
     response.style.display = 'block'
-    // Can use Google Analytics to track these clicks and keep track of feedback
-    // https://developers.google.com/analytics/devguides/collection/analyticsjs/sending-hits
-    //ga('send', 'event', 'Positive Feedback', 'click');
+    //Can use Google Analytics to track these clicks and keep track of feedback
+    //https://developers.google.com/analytics/devguides/collection/analyticsjs/sending-hits
+    ga('send', 'event', 'Positive Feedback', 'click');
   }
   no.onclick = function () {
     options.innerHTML = ''
     response.style.display = 'block'
-    //ga('send', 'event', 'Negative Feedback', 'click');
+    ga('send', 'event', 'Negative Feedback', 'click');
   }
 })()
