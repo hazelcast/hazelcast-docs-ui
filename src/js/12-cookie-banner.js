0c6ca4afbb4b0a41b9adebeea7d704b7b2e9a83c
JakeSCahill <jakecahillgh@gmail.com>
2021-07-12 09:53:26 +0100

    Add 5.0 to banner

--- src/js/12-cookie-banner.js
+++ src/js/12-cookie-banner.js
@@ -3,10 +3,24 @@
 
   var cookieBanner = document.getElementById('cookie')
   var cookieButton = document.getElementById('cookie-close')
+  var latestBanner = document.getElementById('latest-banner')
 
   if (window.localStorage.getItem('docsCookie') !== 'closed') {
     cookieBanner.style.display = 'flex'
-  };
+  }
+
+  const itemStr = window.localStorage.getItem('latestBannerCookie')
+  if (itemStr) {
+    const item = JSON.parse(itemStr)
+    const now = new Date()
+    if (now.getTime() > item.expiry) {
+      window.localStorage.removeItem('latestBannerCookie')
+      latestBanner.style.display = 'flex'
+    }
+  } else {
+    latestBanner.style.display = 'flex'
+  }
+
   cookieButton.addEventListener('click', function () {
     cookieBanner.style.display = 'none'
     window.localStorage.setItem('docsCookie', 'closed')
