e5702c52370b7dd7bbfec7e08e83feb8619a0f0c
JakeSCahill <jakecahillgh@gmail.com>
2022-01-05 10:02:30 +0000

    Cache nav tree results for faster processing

--- src/helpers/is-enterprise.js
+++ src/helpers/is-enterprise.js
@@ -1,8 +1,12 @@
 'use strict'
 
-module.exports = (navUrl, component, { data: { root } }) => {
-  const { contentCatalog /*,page*/ } = root
-  const pages = contentCatalog.findBy({ component: /*page.component.name*/component, family: 'page' })
+module.exports = (navUrl, { data: { root } }) => {
+  const { contentCatalog, page } = root
+  var pages = contentCatalog.navGroup
+  if (!pages) {
+    pages = contentCatalog.findBy({ component: page.component.name, family: 'page' })
+    contentCatalog.navGroup = pages
+  }
   for (let i = 0; i < pages.length; i++) {
     if (pages[i].pub.url === navUrl &&
       pages[i].asciidoc.attributes['page-enterprise'] === 'true') {
