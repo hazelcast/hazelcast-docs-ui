903a8f4543ffe521c84ad506d27fd7eda8becd22
JakeSCahill <jakecahillgh@gmail.com>
2022-01-07 18:20:55 +0000

    Fix bug in sidenav label checker

--- src/helpers/is-enterprise.js
+++ src/helpers/is-enterprise.js
@@ -3,9 +3,12 @@
 module.exports = (navUrl, { data: { root } }) => {
   const { contentCatalog, page } = root
   var pages = contentCatalog.navGroup
-  if (!pages) {
+  var cp = contentCatalog.cp
+  if (!pages || cp !== page.component.name) {
     pages = contentCatalog.findBy({ component: page.component.name, family: 'page' })
     contentCatalog.navGroup = pages
+    contentCatalog.cp = page.component.name
+    console.log(contentCatalog.cp)
   }
   for (let i = 0; i < pages.length; i++) {
     if (pages[i].pub.url === navUrl &&
