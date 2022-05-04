cfa886c8af1e1a44e119122090eee2396fd301e1
JakeSCahill <jakecahillgh@gmail.com>
2022-05-04 10:47:11 +0100

    Enable search by product with query parameters

--- src/js/13-hide-nav.js
+++ src/js/13-hide-nav.js
@@ -3,15 +3,16 @@
 
   var hideNavButton = document.getElementById('hide-nav')
 
-  hideNavButton.addEventListener('click', function () {
-    var nav = document.getElementsByClassName('nav-container')[0]
-
-    if (nav.style.display === 'none') {
-      nav.style.display = 'block'
-      hideNavButton.style.transform = 'none'
-    } else {
-      nav.style.display = 'none'
-      hideNavButton.style.transform = 'rotate(180deg)'
-    }
-  })
+  if (hideNavButton) {
+    hideNavButton.addEventListener('click', function () {
+      var nav = document.getElementsByClassName('nav-container')[0]
+      if (nav.style.display === 'none') {
+        nav.style.display = 'block'
+        hideNavButton.style.transform = 'none'
+      } else {
+        nav.style.display = 'none'
+        hideNavButton.style.transform = 'rotate(180deg)'
+      }
+    })
+  }
 })()
