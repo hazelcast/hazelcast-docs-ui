e9bc9975daaee0c8daf946cdacb2a46b8c39d8a1
JakeSCahill <jakecahillgh@gmail.com>
2022-07-12 12:38:35 +0100

    Add improvements for Serverless launch

--- src/helpers/get-tutorial-pages.js
+++ src/helpers/get-tutorial-pages.js
@@ -1,12 +1,19 @@
 'use strict'
 
-module.exports = ({ data: { root } }) => {
-  const { contentCatalog, page } = root
-  const pages = contentCatalog.findBy({ component: page.component.name, family: 'page' })
+module.exports = (product, { data: { root } }) => {
+  const { contentCatalog } = root
+  const pages = contentCatalog.findBy({ component: 'tutorials', family: 'page' })
   var tutorials = []
   for (let i = 0; i < pages.length; i++) {
-    if (pages[i].asciidoc.attributes['page-layout'] === 'tutorial') {
-      tutorials.push(pages[i])
+    if (product) {
+      if (pages[i].asciidoc.attributes['page-layout'] === 'tutorial' &&
+        pages[i].asciidoc.attributes['page-product'] === 'cloud') {
+        tutorials.push(pages[i])
+      }
+    } else {
+      if (pages[i].asciidoc.attributes['page-layout'] === 'tutorial') {
+        tutorials.push(pages[i])
+      }
     }
   }
   return tutorials
