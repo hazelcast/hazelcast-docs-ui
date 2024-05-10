a5049e7969100dd6b9ad8e9d91e76e43c93c8137
Mykola Fant <mykola.fant@hazelcast.com>
2024-05-10 12:41:54 +0300

    adjust right menu

--- src/js/02-on-this-page.js
+++ src/js/02-on-this-page.js
@@ -27,10 +27,6 @@
 
     var menu = sidebar.querySelector('.toc-menu')
     if (!menu) (menu = document.createElement('div')).className = 'toc-menu'
-
-    var title = document.createElement('h3')
-    title.textContent = ''
-    menu.appendChild(title)
     menu.appendChild(list)
 
     var startOfContent = !document.getElementById('toc') && article.querySelector('h1.page ~ :not(.is-before-toc)')
