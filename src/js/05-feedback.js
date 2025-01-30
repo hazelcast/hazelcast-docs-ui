c57ab56ad0c14a8c1f4656bc4927f7403b730afb
Mykola Fant <mykola.fant@hazelcast.com>
2025-01-30 16:31:51 +0200

    improve feedback

--- src/js/05-feedback.js
+++ src/js/05-feedback.js
@@ -1,8 +1,10 @@
+const feedbackForm = document.getElementById('feedback-form')
+const additionalTextualFeedback = document.getElementById('additional-textual-feedback')
+
 const handleSubmit = (event) => {
   event.preventDefault()
 
   const myForm = event.target
-  console.log('myForm', myForm)
   // eslint-disable-next-line no-undef
   const formData = new FormData(myForm)
 
@@ -16,6 +18,34 @@ const handleSubmit = (event) => {
     .then(() => alert('Thank you for your submission'))
     // eslint-disable-next-line no-undef
     .catch((error) => alert(error))
-};
+}
 
-document.getElementById('feedback-form').addEventListener('submit', handleSubmit)
+const openFormContainer = (containerToOpen, containerToClose, radioToCheck, radioToUncheck) => {
+  feedbackForm.reset()
+  document.getElementById(containerToOpen).classList.remove('hidden')
+  document.getElementById(containerToClose).classList.add('hidden')
+  document.getElementById(radioToCheck).setAttribute('checked', '')
+  document.getElementById(radioToUncheck).removeAttribute('checked')
+}
+
+const showPositiveFeedbackForm = () => {
+  openFormContainer(
+    'positive-feedback-form-container',
+    'negative-feedback-form-container',
+    'yes',
+    'no'
+  )
+}
+
+const showNegativeFeedbackForm = () => {
+  openFormContainer(
+    'negative-feedback-form-container',
+    'positive-feedback-form-container',
+    'no',
+    'yes'
+  )
+}
+
+feedbackForm.addEventListener('submit', handleSubmit)
+document.getElementById('positive-feedback-starter').addEventListener('click', showPositiveFeedbackForm)
+document.getElementById('negative-feedback-starter').addEventListener('click', showNegativeFeedbackForm)
