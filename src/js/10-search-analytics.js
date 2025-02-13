e333ad9deb6540e4c9bf5c55d73a53bdce6e6805
Mykola Fant <mykola.fant@hazelcast.com>
2025-02-13 17:43:42 +0200

    cleanup

--- src/js/10-search-analytics.js
+++ src/js/10-search-analytics.js
@@ -1,15 +1,9 @@
 // eslint-disable-next-line no-undef
 if (Kapa) {
   // eslint-disable-next-line no-undef
-  Kapa('onSearchResultsCompleted', ({ query, searchResult }) => {
-    console.log('SearchResultsCompleted => query : results', query, searchResult)
-    window.dataLayer && window.dataLayer.push('SearchResultsCompleted => query : results', query, searchResult)
-  })
-
-  // eslint-disable-next-line no-undef
-  Kapa('onSearchResultClick', ({ query, searchResult, rank }) => {
-    console.log('SearchResultClick => query : results : rank', query, searchResult, rank)
-    window.dataLayer && window.dataLayer.push('SearchResultClick => query : results : rank', query, searchResult, rank)
+  Kapa('onSearchResultsCompleted', ({ query, searchResults }) => {
+    console.log('SearchResultsCompleted - query : results => ', query, searchResults)
+    window.dataLayer && window.dataLayer.push('SearchResultsCompleted => query : results', query, searchResults)
   })
 } else {
   console.warn('Kapa was not detected!')
