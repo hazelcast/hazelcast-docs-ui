e8852d065fcbff1a6b0f51f90af603e1cd333529
Mykola Fant <mykola.fant@hazelcast.com>
2025-02-18 09:58:49 +0200

    debounce search metrics

--- src/js/10-search-analytics.js
+++ src/js/10-search-analytics.js
@@ -1,10 +1,28 @@
-// eslint-disable-next-line no-undef
-if (Kapa) {
-  // eslint-disable-next-line no-undef
-  Kapa('onSearchResultsCompleted', ({ query, searchResults }) => {
-    console.log('SearchResultsCompleted - query : results => ', query, searchResults)
-    window.dataLayer && window.dataLayer.push('SearchResultsCompleted => query : results', query, searchResults)
+;(function () {
+  document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOMContentLoaded')
+
+    function debounce (func, timeout) {
+      let timer
+      return function (...args) {
+        clearTimeout(timer)
+        timer = setTimeout(() => func.apply(this, args), timeout)
+      }
+    }
+
+    const onSearchResultsCompletedHandler = ({ query, searchResults }) => {
+      console.debug('GTM', ['SearchResultsCompleted', query, searchResults])
+      window.dataLayer && window.dataLayer.push(['SearchResultsCompleted', query, searchResults])
+    }
+    const timeout = 5000
+
+    const onSearchResultsCompletedHandlerDebounced = debounce(onSearchResultsCompletedHandler, timeout)
+
+    if (window.Kapa) {
+      // eslint-disable-next-line no-undef
+      window.Kapa('onSearchResultsCompleted', onSearchResultsCompletedHandlerDebounced)
+    } else {
+      console.warn('Kapa was not detected!')
+    }
   })
-} else {
-  console.warn('Kapa was not detected!')
-}
+})()
