fe0a09cfc99a87f206b9be756ae4f728074e7e20
Mykola Fant <mykola.fant@hazelcast.com>
2025-07-17 16:41:53 +0300

    filter components

--- src/js/08-copy-to-clipboard.js
+++ src/js/08-copy-to-clipboard.js
@@ -1,5 +1,26 @@
 ;(function () {
   'use strict'
+
+  const getOnCopySuccess = (copyButton) => () => {
+    /* Chrome doesn't seem to blur automatically,
+              leaving the button in a focused state. */
+    copyButton.blur()
+    copyButton.dataset.title = 'Copied ✓'
+    setTimeout(function () {
+      copyButton.dataset.title = 'Copy'
+    }, 2000)
+  }
+
+  const getOnCopyError = (copyButton) => () => {
+    copyButton.dataset.title = 'Error'
+  }
+
+  const normalizeBashCode = (code) => {
+    /* Multiline code samples with annotations
+                have extra spaces at the end of lines, which break samples on copy. */
+    return code.replace(/\\\s+$/gm, '\\')
+  }
+
   document.querySelectorAll('pre > code').forEach(function (codeBlock) {
     var sourceTypeBox = document.createElement('div')
     sourceTypeBox.className = 'source-type-box'
@@ -20,38 +41,20 @@
     copyButton.addEventListener('click', function (e) {
       // NOTE: ignore event on pseudo-element
       if (e.currentTarget === e.target) return
-      var bashText = codeBlock.innerText
+      var bashText = normalizeBashCode(codeBlock.innerText)
       // remove '$' from copy to code functionality in code block console
       var check = bashText.charAt(0)
       if (check === '$') {
         var spliceData = bashText.substring(2)
         navigator.clipboard.writeText(spliceData).then(
-          function () {
-            /* Chrome doesn't seem to blur automatically,
-                leaving the button in a focused state. */
-            copyButton.blur()
-            copyButton.dataset.title = 'Copied ✓'
-            setTimeout(function () {
-              copyButton.dataset.title = 'Copy'
-            }, 2000)
-          },
-          function () {
-            copyButton.dataset.title = 'Error'
-          })
+          getOnCopySuccess(copyButton),
+          getOnCopyError(copyButton)
+        )
       } else {
-        navigator.clipboard.writeText(codeBlock.innerText).then(
-          function () {
-            /* Chrome doesn't seem to blur automatically,
-                leaving the button in a focused state. */
-            copyButton.blur()
-            copyButton.dataset.title = 'Copied ✓'
-            setTimeout(function () {
-              copyButton.dataset.title = 'Copy'
-            }, 2000)
-          },
-          function () {
-            copyButton.dataset.title = 'Error'
-          })
+        navigator.clipboard.writeText(bashText).then(
+          getOnCopySuccess(copyButton),
+          getOnCopyError(copyButton)
+        )
       }
     })
     var pre = codeBlock.parentNode
