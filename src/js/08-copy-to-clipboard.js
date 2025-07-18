318df77d6a645210d7c04c731a3b74a6711c81ca
Mykola Fant <mykola.fant@hazelcast.com>
2025-07-18 18:50:37 +0300

    Fix copy code with annotations (#274)
    https://hazelcast.atlassian.net/browse/DOC-534
    
    **BONUSES**:
    - show the word "Copy" on the copy button for a better visibility;
    - prevent code selection;
    - copy code on click everywhere on the code block;
    - fix copy of multiline code starting with `$` (like
    [here](https://docs.hazelcast.com/hazelcast/6.0-snapshot/clients/cplusplus#conan-users)).

--- src/js/08-copy-to-clipboard.js
+++ src/js/08-copy-to-clipboard.js
@@ -1,11 +1,53 @@
 ;(function () {
   'use strict'
+
+  const showCopyToast = () => {
+    const toast = document.createElement('div')
+    toast.className = 'copy-toast'
+    toast.innerHTML = 'Copied to clipboard ✓'
+    document.body.appendChild(toast)
+
+    setTimeout(() => {
+      toast.classList.add('show')
+    }, 100)
+
+    setTimeout(() => {
+      toast.classList.remove('show')
+      setTimeout(() => {
+        document.body.removeChild(toast)
+      }, 300)
+    }, 2500)
+  }
+
+  const getOnCopySuccess = (copyButton) => () => {
+    /* Chrome doesn't seem to blur automatically,
+              leaving the button in a focused state. */
+    copyButton.blur()
+    showCopyToast()
+  }
+
+  const getOnCopyError = (copyButton) => () => {
+    copyButton.dataset.title = 'Error'
+  }
+
+  const normalizeBashCode = (code) => {
+    /* Multiline code samples with annotations
+                have extra spaces at the end of lines, which break samples on copy. */
+    return code.replace(/\\\s+$/gm, '\\')
+  }
+
+  const trimCode = (code) => {
+  /* remove '$' from copy to code functionality in code block console */
+    return code.replace(/^\$\s/gm, '')
+  }
+
   document.querySelectorAll('pre > code').forEach(function (codeBlock) {
     var sourceTypeBox = document.createElement('div')
     sourceTypeBox.className = 'source-type-box'
     var copyButton = document.createElement('a')
     copyButton.className = 'copy-code-button'
     copyButton.dataset.title = 'Copy'
+    copyButton.innerText = 'Copy'
     copyButton.appendChild(document.createElement('i')).className = 'fa-regular fa-copy'
     var dataSource = document.createElement('span')
     dataSource.className = 'data-source'
@@ -17,42 +59,15 @@
     var fadeShadow = document.createElement('span')
     fadeShadow.className = 'fade-shadow'
 
-    copyButton.addEventListener('click', function (e) {
-      // NOTE: ignore event on pseudo-element
-      if (e.currentTarget === e.target) return
-      var bashText = codeBlock.innerText
-      // remove '$' from copy to code functionality in code block console
-      var check = bashText.charAt(0)
-      if (check === '$') {
-        var spliceData = bashText.substring(2)
-        navigator.clipboard.writeText(spliceData).then(
-          function () {
-            /* Chrome doesn't seem to blur automatically,
-                leaving the button in a focused state. */
-            copyButton.blur()
-            copyButton.dataset.title = 'Copied ✓'
-            setTimeout(function () {
-              copyButton.dataset.title = 'Copy'
-            }, 2000)
-          },
-          function () {
-            copyButton.dataset.title = 'Error'
-          })
-      } else {
-        navigator.clipboard.writeText(codeBlock.innerText).then(
-          function () {
-            /* Chrome doesn't seem to blur automatically,
-                leaving the button in a focused state. */
-            copyButton.blur()
-            copyButton.dataset.title = 'Copied ✓'
-            setTimeout(function () {
-              copyButton.dataset.title = 'Copy'
-            }, 2000)
-          },
-          function () {
-            copyButton.dataset.title = 'Error'
-          })
+    codeBlock.parentNode.addEventListener('click', function () {
+      let codeString = normalizeBashCode(codeBlock.innerText)
+      if (codeString.charAt(0) === '$') {
+        codeString = trimCode(codeString)
       }
+      navigator.clipboard.writeText(codeString).then(
+        getOnCopySuccess(copyButton),
+        getOnCopyError(copyButton)
+      )
     })
     var pre = codeBlock.parentNode
     pre.appendChild(sourceTypeBox)
