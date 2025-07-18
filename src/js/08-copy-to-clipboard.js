e28bc52d97772e2670b6382e3c241e15de8a7939
Mykola Fant <mykola.fant@hazelcast.com>
2025-07-18 15:29:05 +0300

    redesign code blocks

--- src/js/08-copy-to-clipboard.js
+++ src/js/08-copy-to-clipboard.js
@@ -1,14 +1,29 @@
 ;(function () {
   'use strict'
 
+  const showCopyToast = () => {
+    const toast = document.createElement('div')
+    toast.className = 'copy-toast'
+    toast.innerHTML = 'Copied to clipboard ✓'
+    document.body.appendChild(toast)
+
+    setTimeout(() => {
+      toast.classList.add('show')
+    }, 10)
+
+    setTimeout(() => {
+      toast.classList.remove('show')
+      setTimeout(() => {
+        document.body.removeChild(toast)
+      }, 300)
+    }, 2000)
+  }
+
   const getOnCopySuccess = (copyButton) => () => {
     /* Chrome doesn't seem to blur automatically,
               leaving the button in a focused state. */
     copyButton.blur()
-    copyButton.dataset.title = 'Copied ✓'
-    setTimeout(function () {
-      copyButton.dataset.title = 'Copy'
-    }, 2000)
+    showCopyToast()
   }
 
   const getOnCopyError = (copyButton) => () => {
@@ -21,12 +36,18 @@
     return code.replace(/\\\s+$/gm, '\\')
   }
 
+  const trimCode = (code) => {
+  /* remove '$' from copy to code functionality in code block console */
+    return code.replace(/^\$\s/gm, '')
+  }
+
   document.querySelectorAll('pre > code').forEach(function (codeBlock) {
     var sourceTypeBox = document.createElement('div')
     sourceTypeBox.className = 'source-type-box'
     var copyButton = document.createElement('a')
     copyButton.className = 'copy-code-button'
     copyButton.dataset.title = 'Copy'
+    copyButton.innerText = 'Copy'
     copyButton.appendChild(document.createElement('i')).className = 'fa-regular fa-copy'
     var dataSource = document.createElement('span')
     dataSource.className = 'data-source'
@@ -38,24 +59,15 @@
     var fadeShadow = document.createElement('span')
     fadeShadow.className = 'fade-shadow'
 
-    copyButton.addEventListener('click', function (e) {
-      // NOTE: ignore event on pseudo-element
-      if (e.currentTarget === e.target) return
-      var bashText = normalizeBashCode(codeBlock.innerText)
-      // remove '$' from copy to code functionality in code block console
-      var check = bashText.charAt(0)
-      if (check === '$') {
-        var spliceData = bashText.substring(2)
-        navigator.clipboard.writeText(spliceData).then(
-          getOnCopySuccess(copyButton),
-          getOnCopyError(copyButton)
-        )
-      } else {
-        navigator.clipboard.writeText(bashText).then(
-          getOnCopySuccess(copyButton),
-          getOnCopyError(copyButton)
-        )
+    codeBlock.parentNode.addEventListener('click', function () {
+      let codeString = normalizeBashCode(codeBlock.innerText)
+      if (codeString.charAt(0) === '$') {
+        codeString = trimCode(codeString)
       }
+      navigator.clipboard.writeText(codeString).then(
+        getOnCopySuccess(copyButton),
+        getOnCopyError(copyButton)
+      )
     })
     var pre = codeBlock.parentNode
     pre.appendChild(sourceTypeBox)
