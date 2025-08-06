cfd53f71fb45e2faee53e5f1f2a6bfa0612fb483
Mykola Fant <mykola.fant@hazelcast.com>
2025-08-06 13:22:31 +0300

    fix versions

--- src/helpers/get-facets-from-components.js
+++ src/helpers/get-facets-from-components.js
@@ -1,17 +1,14 @@
 'use strict'
 
 module.exports = (components, page) => {
-  const currentComponentVersion = `${page.component?.title}_${page.displayVersion}`
   return JSON.stringify(components.reduce((acc, component) => {
     if (component.versions?.length) {
       const title = component.versions?.[0].title
-      acc[title] = component.versions
+      const versions = component.versions
         .slice(0, page.attributes['last-versions-count'])
-        .reduce((versionAcc, { displayVersion }) => {
-          versionAcc[displayVersion] = currentComponentVersion === `${title}_${displayVersion}`
-          return versionAcc
-        }, {})
+        .map(({ displayVersion }) => displayVersion)
+      acc.push({ title, versions })
     }
     return acc
-  }, {}))
+  }, []))
 }
