402ca296a0013bd31e92ec4f164972daea2c6fda
Mykola Fant <mykola.fant@hazelcast.com>
2025-08-11 17:34:30 +0300

    fix Rob's issues

--- src/helpers/get-facets-from-components.js
+++ src/helpers/get-facets-from-components.js
@@ -4,10 +4,11 @@ module.exports = (components, page) => {
   return JSON.stringify(components.reduce((acc, component) => {
     if (component.versions?.length) {
       const title = component.versions?.[0].title
+      const latestVersion = component.latest.displayVersion
       const versions = component.versions
         .slice(0, page.attributes['last-versions-count'])
         .map(({ displayVersion }) => displayVersion)
-      acc.push({ title, versions })
+      acc.push({ title, versions, latestVersion })
     }
     return acc
   }, []))
