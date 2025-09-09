4cdf7dbd5e053c92eefabb3e5dd8e55a2487b047
Mykola Fant <mykola.fant@hazelcast.com>
2025-09-09 22:29:56 +0300

    add padding for beta label

--- src/helpers/get-facets-from-components.js
+++ src/helpers/get-facets-from-components.js
@@ -4,11 +4,12 @@ module.exports = (components, page) => {
   return JSON.stringify(components.reduce((acc, component) => {
     if (component.versions?.length) {
       const title = component.versions?.[0].title
+      const name = component.name
       const latestVersion = component.latest.displayVersion
       const versions = component.versions
         .slice(0, page.attributes['last-versions-count'])
         .map(({ displayVersion }) => displayVersion)
-      acc.push({ title, versions, latestVersion })
+      acc.push({ name, title, versions, latestVersion })
     }
     return acc
   }, []))
