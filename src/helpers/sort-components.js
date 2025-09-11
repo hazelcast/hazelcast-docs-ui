0717fe4c14aff02ca5f72470ec5fcbae6f847212
Mykola Fant <mykola.fant@hazelcast.com>
2025-09-11 16:26:54 +0300

    add version exclude

--- src/helpers/sort-components.js
+++ src/helpers/sort-components.js
@@ -1,6 +1,23 @@
 'use strict'
 
-module.exports = (collection, property, orderSpec) => {
+const excludeComponentVersions = (targetCollection, page) => {
+  if (page.attributes['excluded-versions']) {
+    const excludedComponentVersions = page.attributes['excluded-versions']
+      .split(',').map((it) => it.trim())
+    excludedComponentVersions.forEach((componentVersion, index) => {
+      const [componentName, versionName] = componentVersion.split(':')
+      const component = targetCollection.find(({ name }) => name === componentName)
+      if (!component) {
+        console.warn(`No component found for excluded-versions[${index}] -> ${componentVersion}`)
+      } else {
+        component.versions = component.versions.filter(({ version }) => version !== versionName)
+      }
+    })
+  }
+  return targetCollection
+}
+
+module.exports = (collection, property, orderSpec, { data: { root } }) => {
   if (orderSpec == null || orderSpec === '*') return Object.values(collection)
   const sourceCollection = Object.values(collection).reduce((accum, it) => accum.set(it[property], it), new Map())
   const order = orderSpec
@@ -12,7 +29,7 @@ module.exports = (collection, property, orderSpec) => {
     })
   const restIdx = order.indexOf('*')
   if (~restIdx) order.splice(restIdx, 1)
-  const targetCollection = order.reduce((accum, key) => {
+  let targetCollection = order.reduce((accum, key) => {
     if (sourceCollection.has(key)) {
       accum.push(sourceCollection.get(key))
       sourceCollection.delete(key)
@@ -20,5 +37,6 @@ module.exports = (collection, property, orderSpec) => {
     return accum
   }, [])
   if (~restIdx) targetCollection.splice(restIdx, 0, ...sourceCollection.values())
+  targetCollection = excludeComponentVersions(targetCollection, root.page)
   return targetCollection
 }
