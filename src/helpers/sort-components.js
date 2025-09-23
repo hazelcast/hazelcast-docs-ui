d3f1e775072987c68a5c25d55ca231a120d50448
Mykola Fant <mykola.fant@hazelcast.com>
2025-09-23 22:28:28 +0300

    exclude page versions

--- src/helpers/sort-components.js
+++ src/helpers/sort-components.js
@@ -1,30 +1,6 @@
 'use strict'
 
-// filter out excluded versions
-const excludeComponentVersions = (targetCollection, page) => {
-  if (page.attributes['excluded-versions']) {
-    const excludedComponentVersions = page.attributes['excluded-versions']
-      .split(',').map((it) => it.trim())
-    // console.debug('[DEBUG:SORT_COMPONENTS] excluding versions: ', excludedComponentVersions)
-    excludedComponentVersions.forEach((componentVersion, index) => {
-      const [componentName, versionName] = componentVersion.split(':')
-      const component = targetCollection.find(({ name }) => name === componentName)
-      if (!component) {
-        console.warn(`No component found for excluded-versions[${index}] -> ${componentVersion}`)
-      } else {
-        component.versions = component.versions.map((nextVersion) => {
-          return {
-            ...nextVersion,
-            isHidden: nextVersion.version === versionName,
-          }
-        })
-      }
-    })
-  }
-  return targetCollection
-}
-
-module.exports = (collection, property, orderSpec, { data: { root } }) => {
+module.exports = (collection, property, orderSpec) => {
   if (orderSpec == null || orderSpec === '*') return Object.values(collection)
   const sourceCollection = Object.values(collection).reduce((accum, it) => accum.set(it[property], it), new Map())
   const order = orderSpec
@@ -36,7 +12,7 @@ module.exports = (collection, property, orderSpec, { data: { root } }) => {
     })
   const restIdx = order.indexOf('*')
   if (~restIdx) order.splice(restIdx, 1)
-  let targetCollection = order.reduce((accum, key) => {
+  const targetCollection = order.reduce((accum, key) => {
     if (sourceCollection.has(key)) {
       accum.push(sourceCollection.get(key))
       sourceCollection.delete(key)
@@ -44,7 +20,5 @@ module.exports = (collection, property, orderSpec, { data: { root } }) => {
     return accum
   }, [])
   if (~restIdx) targetCollection.splice(restIdx, 0, ...sourceCollection.values())
-  targetCollection = excludeComponentVersions(targetCollection, root.page)
-  // console.debug('[DEBUG:SORT_COMPONENTS] targetCollection: ', targetCollection)
   return targetCollection
 }
