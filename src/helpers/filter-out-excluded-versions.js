21377358f8b4620271e19f8c2f250149245edded
Mykola Fant <mykola.fant@hazelcast.com>
2025-09-24 12:25:36 +0300

    Exclude page versions (#279)
    <img width="836" height="383" alt="image"
    src="https://github.com/user-attachments/assets/50c79ca3-6c77-477c-b7c1-f59b770b3a83"
    />

---
+++ src/helpers/filter-out-excluded-versions.js
@@ -0,0 +1,20 @@
+'use strict'
+
+module.exports = ({ data: { root } }) => {
+  let versions = root.page.versions
+  const excludedVersions = root.page.attributes['excluded-versions']
+  const currentComponentName = root.page.component.name
+  if (excludedVersions) {
+    const excludedComponentVersions = excludedVersions.split(',').map((it) => it.trim())
+    // console.debug('[DEBUG:SORT_COMPONENTS] excluding versions: ', excludedComponentVersions)
+    excludedComponentVersions.forEach((componentVersion) => {
+      const [componentName, excludedVersionName] = componentVersion.split(':')
+      if (currentComponentName === componentName) {
+        versions = versions.filter(({ version }) => {
+          return version !== excludedVersionName
+        })
+      }
+    })
+  }
+  return versions
+}
