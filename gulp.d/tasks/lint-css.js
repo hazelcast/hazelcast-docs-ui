74a3eca4b2310b08ab7689a4ecfcd71129422f18
Mykola Fant <mykola.fant@hazelcast.com>
2025-10-13 12:11:55 +0300

    upgrade stylelint

--- gulp.d/tasks/lint-css.js
+++ gulp.d/tasks/lint-css.js
@@ -1,10 +1,16 @@
 'use strict'
 
-const stylelint = require('gulp-stylelint')
+const stylelint = require('gulp-stylelint-esm')
 const vfs = require('vinyl-fs')
 
 module.exports = (files) => (done) =>
   vfs
     .src(files)
-    .pipe(stylelint({ reporters: [{ formatter: 'string', console: true }], failAfterError: true }))
+    .pipe(stylelint.default({
+      reporters: [
+        { formatter: 'string', console: true },
+      ],
+      failAfterError: true,
+      fix: false,
+    }))
     .on('error', done)
