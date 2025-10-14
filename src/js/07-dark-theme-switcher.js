f0925c43f9465a1cac4ad68519bd4a2e01eb824c
Mykola Fant <mykola.fant@hazelcast.com>
2025-10-14 13:01:29 +0300

    style switcher

--- src/js/07-dark-theme-switcher.js
+++ src/js/07-dark-theme-switcher.js
@@ -3,7 +3,12 @@
 
   const lightThemeOnBtn = document.getElementById('lightModeOnBtn')
   const darkThemeOnBtn = document.getElementById('darkModeOnBtn')
+
+  const isDarkModePreferredBySystem = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+
   const colorScheme = document.querySelector('meta[name=color-scheme]')
+
+  colorScheme.content = isDarkModePreferredBySystem ? 'dark' : 'light'
 
   if (lightThemeOnBtn && darkThemeOnBtn && colorScheme) {
     lightThemeOnBtn.addEventListener('click', () => {
