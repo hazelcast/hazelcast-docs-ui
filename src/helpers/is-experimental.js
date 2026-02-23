3f62ae409643175cce9a8fd655b9221c4607741a
fantkolja <fantkolja@gmail.com>
2026-02-23 17:02:39 +0200

    rename metrics to clients

---
+++ src/helpers/is-experimental.js
@@ -0,0 +1,19 @@
+'use strict'
+
+module.exports = (navUrl, { data: { root } }) => {
+  const { contentCatalog, page } = root
+  if (!contentCatalog) return false
+  let pages = contentCatalog.navGroup
+  const cp = contentCatalog.cp
+  if (!pages || cp !== page.component.name) {
+    pages = contentCatalog.findBy({ component: page.component.name, family: 'page' })
+    contentCatalog.navGroup = pages
+    contentCatalog.cp = page.component.name
+  }
+  for (let i = 0; i < pages.length; i++) {
+    if (pages[i].pub.url === navUrl &&
+      pages[i].asciidoc.attributes['page-experimental'] === 'true') {
+      return true
+    }
+  }
+}
